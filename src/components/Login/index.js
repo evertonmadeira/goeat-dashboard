5317bd9b2bb4ac3f0089a25f0e9856cc90cd8a44
Everton Madeira <everton.comp@gmail.com>
2020-04-01 21:37:02 -0300

    Commit #1

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,17 +2,6 @@ import React, { Component } from 'react';
 
 import { Container, Window, Form } from './styles';
 
-export default class Login extends Component {
-  render() {
-    return (
-      <Container>
-        <Window>
-          <Form/>
-          <Form/>
-        </Window>
-      </Container>
-      
-      )
-
-  }
+export default function Login() {
+  return null
 }
